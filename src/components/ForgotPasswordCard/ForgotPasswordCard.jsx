07083065c57931d70a6f19cd0b490d7d77b68f20
manishrajbhar1998 <manishrajbhar379@gmail.com>
2025-07-11 00:09:51 +0530

    forgotpassword api start

--- src/components/ForgotPasswordCard/ForgotPasswordCard.jsx
+++ src/components/ForgotPasswordCard/ForgotPasswordCard.jsx
@@ -7,20 +7,22 @@ import forgotPassword from './../../assets/images/login-register.jpg';
 import logo from './../../assets/images/swastha-mitra-logo2.png';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import { api } from '../../apis/api';
+import { POST_FORGOT_PASSWORD } from '../../constant/config';
+import { useLoading } from '../../context/LoadingContext/LoadingContext';
+import { toast } from 'react-toastify';
 
 
 const LoginSchema = Yup.object().shape({
-    phone: Yup.string()
-        .matches(/^\d{10}$/, 'Phone number must be 10 digits')
-        .required('Phone number is required'),
+
     email: Yup.string()
         .required('Email is required')
         .matches(
             /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
             'Invalid email format'
         ),
-    password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long'),
-    confirmPassword: Yup.string().required('Confirm Password is required').min(6, 'Confirm Password must be at least 6 characters long').oneOf([Yup.ref('password'), null], 'Password and Confirm Password do not match'),
+    // password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long'),
+    // confirmPassword: Yup.string().required('Confirm Password is required').min(6, 'Confirm Password must be at least 6 characters long').oneOf([Yup.ref('password'), null], 'Password and Confirm Password do not match'),
 });
 
 const ForgotPasswordCard = () => {
@@ -32,8 +34,28 @@ const ForgotPasswordCard = () => {
         resolver: yupResolver(LoginSchema),
     });
 
-    const onSubmit = (data) => {
+    const { loading, setLoading } = useLoading();
+
+    const onSubmit = async (data) => {
         console.log(data);
+        debugger
+
+        const reqBody = {
+            email: data.email,
+        };
+
+        try {
+            setLoading(true)
+            const response = await api.post(POST_FORGOT_PASSWORD, reqBody);
+            if (response?.data) {
+                toast.success(response.data.data.message);
+                setLoading(false)
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error("invalid userid ", error);
+            // toast.error(error?.response?.data?.errors[0])
+        }
     };
 
     // const navigate = useNavigate()
@@ -52,13 +74,13 @@ const ForgotPasswordCard = () => {
                 </Box>
             </Box>
             <Typography className='welcome-text'>
-                <Typography component="span">Set Your Password, </Typography>
-                Please create a strong password to secure your account.
+                {/* <Typography component="span">Set Your Password, </Typography> */}
+                {/* Please create a strong password to secure your account. */}
             </Typography>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <TextField
                     fullWidth
-                    label="Email"
+                    label="Enter Register Email Id"
                     type="email"
                     margin="normal"
                     {...register('email')}
@@ -66,7 +88,7 @@ const ForgotPasswordCard = () => {
                     helperText={errors.email?.message}
                 />
 
-                <TextField
+                {/* <TextField
                     fullWidth
                     label="Password"
                     type="password"
@@ -83,10 +105,10 @@ const ForgotPasswordCard = () => {
                     {...register('confirmPassword')}
                     error={!!errors.password}
                     helperText={errors.confirmPassword?.message}
-                />
+                /> */}
 
                 <Button fullWidth variant="contained" color="primary" type="submit" sx={{ marginTop: "10px" }}>
-                    Set Password
+                    Submit
                 </Button>
 
             </form>
