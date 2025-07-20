a7d9a054b2bd86ddd6768156b4214358f30bd682
prem.c <prem.c@orosoft.com>
2025-07-20 14:44:17 +0530

    Live API

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 window._env = {
 
-    API_PATH: "http://localhost:9001",
-    // API_PATH: "https://www.swasthmitra.in/swasthyamitra",
+    // API_PATH: "http://localhost:9001",
+    API_PATH: "https://www.swasthmitra.in/swasthyamitra",
 
 };
