c9970270a1c5e7af4d7a44c97635c3b29be57d41
prem.c <prem.c@orosoft.com>
2025-08-26 16:09:16 +0530

    razorPay live api deplying

--- src/components/IndividualPlan/IndividualPlan.jsx
+++ src/components/IndividualPlan/IndividualPlan.jsx
@@ -47,8 +47,8 @@ const IndividualPlan = () => {
         }
         const cleanAmount = typeof amount === 'string' ? amount.replace(/[^\d.]/g, '') : amount;
         const options = {
-            key: "rzp_test_m7kwYdRW44PWYw", //Razorpay Test key 
-            // key: "rzp_live_y3M1CykMXog8r2", // Razorpay live key
+            // key: "rzp_test_m7kwYdRW44PWYw", //Razorpay Test key 
+            key: "rzp_live_y3M1CykMXog8r2", // Razorpay live key
             amount: Number(cleanAmount) * 100, // Amount in paise
             currency: "INR",
             name: "Swasth Mitra",
@@ -146,7 +146,6 @@ const IndividualPlan = () => {
             numofChild: '',
             children: ''
         };
-        console.log('Form Payload:', payload);
         // If payment failed, do not hit API
         if (paymentInfo.paymentStatus === 'FAILED') {
             toast.error('Payment failed! Your plan purchase was unsuccessful. Please try again or check your payment method.');
