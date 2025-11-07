2fc571263a114b1a89f0d14f0c6a7c4bf8d6bb88
prem.c <prem.c@orosoft.com>
2025-11-07 23:16:39 +0530

    duplicate mobile number permitted

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -41,7 +41,7 @@ const CustomRadio = styled(Radio)({
 const LoginSchema = Yup.object().shape({
     firstName: Yup.string().required('First name is required'),
     lastName: Yup.string().required('Last name is required'),
-    mobile: Yup.string().matches(/^\d{10}$/).notRequired(),
+    mobile: Yup.string().matches(/^\d{10}$/, 'Phone number must be 10 digits').required('Phone number is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     confirmPassword: Yup.string()
